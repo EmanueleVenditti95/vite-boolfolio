e5b0d0af436db6b9b025115a7e10319b7c783a6d
EmanueleVenditti95 <vendittiema@gmail.com>
2024-01-15 16:03:22 +0100

    Add - ProjectCard component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card">
+        <div class="content">
+            <h2>{{ project.title }}</h2>
+            <p>{{ project.description }}</p> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    padding: 10px;
+    flex-basis: calc(100%/4);
+    .content {
+        padding: 20px;
+        background-color: rgb(205, 202, 202,0.5);
+        border-radius: 10px;
+    }
+}
+</style>
--- src/views/projects/Index.vue
+++ src/views/projects/Index.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="container">
-        <h1>Progetti</h1>
+        <h1>Progetti :</h1>
+        <div class="card">
+            <ProjectCard v-for="project in projects" :project="project" :key="project.id"/>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ProjectCard from '../../components/ProjectCard.vue';
 
 export default {
     data(){
@@ -13,11 +17,15 @@ export default {
             projects:[],
         }
     },
+    components: {
+        ProjectCard
+    },
     methods: {
         fetchProjects() {
             axios.get('http://127.0.0.1:8000/api/projects')
             .then((response) => {
                 console.log(response);
+                this.projects = response.data.results;
             })
         }
     },
@@ -28,5 +36,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+    .card {
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
 
 </style>
