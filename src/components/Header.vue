c6c538f3db377b1664734c7a6ad888cb52c6f65d
EmanueleVenditti95 <vendittiema@gmail.com>
2024-01-16 15:56:49 +0100

    Add - header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<template>
+    <div class="container">
+        <ul class="d-flex">
+            <li>
+                <router-link :to="{ name : 'home'}">Home</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name : 'projects'}">Progetti</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .d-flex {
+        display: flex;
+        gap: 10px;
+    }
+</style>
